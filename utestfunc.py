154cac14f3fb34f512f5e5629ad3865d0a4929aa
Ewentry12 <31484103+Ewentry12@users.noreply.github.com>
2017-08-30 20:17:07 +0300

    Add files via upload

---
+++ utestfunc.py
@@ -0,0 +1,30 @@
+#import funcchange
+#import this
+#funcchange.setx(22)
+#print(funcchange.setx(22))
+var=99
+def local():
+    var=0
+def glob1():
+	global var 
+var += 1
+def glob2():
+    var 
+var+=1
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
