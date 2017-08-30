cc11028230d9dc0a4d37b44c77783f507925e16b
Ewentry12 <31484103+Ewentry12@users.noreply.github.com>
2017-08-30 20:21:09 +0300

    Add files via upload

---
+++ Test2.py
@@ -0,0 +1,16 @@
+
+import this
+import utestfunc
+print(utestfunc.var)
+this.test()
+#def maker(N):
+#	def action(X):
+#		return X**N
+#	return action
+#h=maker(2)
+#print(h(1000))
+#def maker(N):
+#	return lambda X:X**N
+#h=maker(2)
+#print(h(1000))
+
